2c05a6be62d5c6a58dce6b86e7027e7bc7b7d0e7
Ymaru <dyun022@gmail.com>
2022-02-04 15:06:15 +0900

    navi bar

--- front-end/src/layouts/MainLayout.vue
+++ front-end/src/layouts/MainLayout.vue
@@ -15,6 +15,12 @@
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
@@ -32,6 +38,17 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          <!-- <div>Title</div> -->
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
